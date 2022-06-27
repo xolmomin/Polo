685c2ba6e090f02cdc13f4635a38d44c223b597c
asadbek <asadbekmirtazayev0@gmail.com>
2022-06-27 19:59:02 +0500

    urls.py changed

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from app.views import index, login_page
 urlpatterns = [
     path('', index, name='index'),
     path('login/', login_page, name='login_page'),
+    path('register/', login_page, name='login_page'),
 ]
